9c5bacd009bbfe3fc73e23f8f4249945e701648d
informatica@asapespu.com <cqLmEG8KKZLVzY6>
2021-06-25 12:55:24 -0600

    Actualizacion 25062021

---
+++ app/inc/registro_respuestas.php
@@ -0,0 +1,3 @@
+<?php
+$respuesta_comision=$_POST['observacion_com'];
+$respuesta_gerencia=$_POST['respuesta_ge'];
--- app/respuestas.php
+++ app/respuestas.php
@@ -1,46 +1,40 @@
 <?php
 session_start();
-
 if (!empty($_SESSION['GUSER'])) {/*#1*/
     $nombre =  $_SESSION['GNOMBRE'];
     $tipousuario = $_SESSION['GTUSUARIO'];//VALIDAR TIPOS DE USUARIO
-
     require('db/conexion.php');
     ?>
-
     <html>
-  <head>
-    <title>Consulta</title>
-    <link rel="stylesheet" href="css/dashboard.css">
-    <link rel="stylesheet" href="css/posiciones.css">
-    </style>
-    <script src="js/scrip.js"></script>
-  </head>
-  <body><div class="area">
-
-  <div class="padre">
-  <div class="hijo">
-  </div>
-  </div>
-
+    <head>
+        <title>Consulta</title>
+        <link rel="stylesheet" href="css/dashboard.css">
+        <link rel="stylesheet" href="css/posiciones.css">
+        <script src="js/scrip.js"></script>
+    </head>
+    <body>
+    <div class="area">
         <div class="padre">
             <div class="hijo">
 
-
+            </div>
+        </div>
+        <div class="padre">
+            <div class="hijo">
                 <?php
-
-                if ($records = mysqli_query($connect, "select CODIGO, EXPEDIENTE, concat(PRIMER_NOMBRE,' ',SEGUNDO_NOMBRE,' ',PRIMER_APELLIDO,' ',SEGUNDO_APELLIDO) as NOMBRE, SUGERENCIA, FECHARECIBIDO, OBSERVACION_COMISION, RESPUESTA_GERENCIA from sugerencia_aceptada order by IDSUGERENCIA desc")) {
+                if ($records = mysqli_query($connect, "select IDSUGERENCIA,CODIGO, EXPEDIENTE, concat(PRIMER_NOMBRE,' ',SEGUNDO_NOMBRE,' ',PRIMER_APELLIDO,' ',SEGUNDO_APELLIDO) as NOMBRE, SUGERENCIA, FECHARECIBIDO, OBSERVACION_COMISION, RESPUESTA_GERENCIA from sugerencia_aceptada order by IDSUGERENCIA desc")) {
                 $row_ = mysqli_num_rows($records);
                 if ($row_ === 0) {
                     echo "<h3>NO EXISTEN DATOS</h3>";
-                }else{
-                $contador = 0;
-                ?>
+                } else {
+                    $contador = 0;
+                    ?>
 
                 <table border='1'>
                     <caption><h1>INGRESO DE RESPUESTAS - BOLETA DE SUGERENCIA</h1></caption>
                     <tr>
-                        <th>#</th>
+                        <th>No.</th>
+                        <th>ID SUGERENCIA DB*</th>
                         <th>CODIGO</th>
                         <th>EXPEDIENTE</th>
                         <th>NOMBRE COMPLETO</th>
@@ -53,47 +47,44 @@ if (!empty($_SESSION['GUSER'])) {/*#1*/
                     <?php
                     while (($reg = mysqli_fetch_array($records))){
                         $contador++;
-                        echo "<tr> <form>";
-                        echo "<th>".$contador."</th>";
-                        echo "<th>".$reg['CODIGO']."</th>";
-                        echo "<th>".$reg['EXPEDIENTE']."</th>";
-                        echo "<th>".$reg['NOMBRE']."</th>";
-                        echo "<th>".$reg['SUGERENCIA']."</th>";
-                        echo "<th>".date('d/m/y', strtotime($reg['FECHARECIBIDO']))."</th>";
-
+                        echo "<td>".$contador."</td>";
+                        echo "<td>".$reg['IDSUGERENCIA']."</td>";
+                        echo "<td>".$reg['CODIGO']."</td>";
+                        echo "<td>".$reg['EXPEDIENTE']."</td>";
+                        echo "<td>".$reg['NOMBRE']."</td>";
+                        echo "<td>".$reg['SUGERENCIA']."</td>";
+                        echo "<td>".date('d/m/y', strtotime($reg['FECHARECIBIDO']))."</td>";
                         if (empty($reg['OBSERVACION_COMISION'])){
-                            echo "<th><textarea rows='5'></textarea></th>";
+                            echo "<td><textarea rows='5' id='observacion_com'></textarea></td>";
                         } else{
-                            echo "<th>".$reg['OBSERVACION_COMISION']."</th>";
+                            echo "<td>".$reg['OBSERVACION_COMISION']."</td>";
                         }
                         if (empty($reg['RESPUESTA_GERENCIA'])){
-                            echo "<th><textarea rows='5'></textarea></th>";
+                            echo "<td><textarea rows='5' id='respuesta_ge'></textarea></td>";
                         }else{
-                            echo "<th>".$reg['RESPUESTA_GERENCIA']."</th>";
+                            echo "<td>".$reg['RESPUESTA_GERENCIA']."</td>";
                         }
 
-                        echo "<th><input type='submit' value='Guardar Respuesta - Asociado'></th></form>";
-
+                        if(!empty($reg['OBSERVACION_COMISION'] AND $reg['RESPUESTA_GERENCIA'])){
+                            echo "<td><input type='submit' value='Guardar Respuesta - Asociado' disabled='true'></td>";
+                        } else {
+                            echo "<td><input type='submit' value='Guardar Respuesta - Asociado'></td></form>";
+                        }
                         echo "</tr>";
-
                         echo "</tr>";
                     }
-
                     echo " </table>";
                 }
-                }
-                    ?>
+                }?>
             </div>
         </div>
         <br>
-        </div>
-        <?php include 'inc/menu.php' ?>
-  </body>
+    </div>
+    <?php include 'inc/menu.php' ?>
+    </body>
     </html>
-
 <?php
-
-/*#1*/}else{/*#1*/
-               header('location: http://localhost/asapespu/app/index.php');
+    /*#1*/}else{/*#1*/
+    header('location: http://localhost/asapespu/app/index.php');
     /*#1*/}
- ?>
+?>
